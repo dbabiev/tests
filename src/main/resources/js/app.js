139392d40054cb053ee8727a07d5d434f5c42168
dmitry <dbabiev@naumen.ru>
2014-12-30 19:08:26 +0400

    corrected paging

--- src/main/resources/js/app.js
+++ src/main/resources/js/app.js
@@ -3,8 +3,10 @@ app.controller('ProductController', function($scope, $filter){
 
     $scope.products = getProducts();
     $scope.pageSize = 3;
+    $scope.reverse = false;
 
-    $scope.sort = function (sortBy) {
+    $scope.sort = function (sortBy){
+        $scope.filteredList = $scope.products;
         $scope.columnToOrder = sortBy;
         $scope.currentPage = 0;
 
@@ -14,7 +16,7 @@ app.controller('ProductController', function($scope, $filter){
     };
 
     $scope.pagination = function () {
-        $scope.products = $scope.paged($scope.filteredList, $scope.pageSize);
+        $scope.productsByPage = $scope.paged($scope.filteredList, $scope.pageSize);
     };
 
     $scope.setPage = function () {
@@ -26,7 +28,7 @@ app.controller('ProductController', function($scope, $filter){
     };
 
     $scope.lastPage = function () {
-        $scope.currentPage = $scope.products.length - 1;
+        $scope.currentPage = $scope.productsByPage.length - 1;
     };
 
     $scope.paged = function (valLists, pageSize) {
@@ -40,6 +42,22 @@ app.controller('ProductController', function($scope, $filter){
         }
         return retVal;
     };
+
+    $scope.range = function (input, total) {
+        var ret = [];
+        if (!total) {
+            total = input;
+            input = 0;
+        }
+        for (var i = input; i < total; i++) {
+            if (i != 0 && i != total - 1) {
+                ret.push(i);
+            }
+        }
+        return ret;
+    };
+
+    $scope.sort('name');
 })
 
 function getProducts(){
@@ -48,6 +66,8 @@ function getProducts(){
         {title: 'Окунь', group: 'рыба', proteins: 35, fats: 64, carbos: 57, calories: 37},
         {title: 'Рис', group: 'крупы', proteins: 56, fats: 36, carbos: 35, calories: 57},
         {title: 'Картофель', group: 'овощи', proteins: 87, fats: 43, carbos: 34, calories: 54},
-        {title: 'Гречка', group: 'крупы', proteins: 76, fats: 45, carbos: 23, calories: 98}
+        {title: 'Гречка', group: 'крупы', proteins: 76, fats: 45, carbos: 23, calories: 98},
+        {title: 'Яблоко', group: 'фрукты', proteins: 74, fats: 83, carbos: 25, calories: 43},
+        {title: 'Сом', group: 'рыба', proteins: 75, fats: 43, carbos: 23, calories: 85}
     ]
 }
